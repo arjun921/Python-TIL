658bad0b1fbfca7da5522f5990c4494174bda5ca
Arjun S <arjun.mi1@iiitmk.ac.in>
2016-12-10 07:42:07 +0530

    Done 10_December_2016 Problem

---
+++ course/conditionals.py
@@ -0,0 +1,8 @@
+import sys
+
+a=sys.argv[1]
+
+cp='coldplay'
+
+print("ahuuuunnnnununn" if a==cp else "booooooo" )
+
---
+++ dailyPrograms/10_December_2016_Odd_Or_Even.py
@@ -0,0 +1,7 @@
+number = int(input('Enter number to check if odd or even'))
+
+if number%2==0:
+	print("Number is even")
+else:
+	print("Number is odd")
+
