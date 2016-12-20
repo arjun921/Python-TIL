77e387628a412155cc1b0640f6f1bbc8c3ad2a6d
Arjun S <arjun.mi1@iiitmk.ac.in>
2016-12-20 10:41:23 +0530

    Fixed bug in prime progra

--- dailyPrograms/19_December_2016_Check_Primality_Functions.py
+++ dailyPrograms/19_December_2016_Check_Primality_Functions.py
@@ -1,12 +1,12 @@
-checkPrime():
+def checkPrime():
 	n = int(input("Enter number to check if prime or not"))
-	flag = 0
 	b = n/2
 	for i in range(2,int(b)):
 		if n%i==0:
-                        flag = 1
-                        break
-        return flag;
+			return False
+	return True
 
-checkPrime()
-print("{} is prime".format(n) if flag == 0 else "Number is not prime")
+if checkPrime():
+	print('Is prime')
+else:
+	print('Nop')
