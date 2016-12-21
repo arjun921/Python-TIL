de49d29126b1defea734eb1408f541304ba6293e
Arjun S <arjun.mi1@iiitmk.ac.in>
2016-12-21 23:23:13 +0530

    Done 21_December_2016 Program Func

--- dailyPrograms/21_December_2016_Fibonacci.py
+++ dailyPrograms/21_December_2016_Fibonacci.py
@@ -1,9 +1,13 @@
-n = int(input('Fibonacci till :'))
-a,b = 1,1
-for i in range(n-1):
-	#a,b = b,a+b
-	#vs
-	#a = b
-	#b = a+b
-	a,b = b,a+b
+def fibon(n):
+	a,b = 1,1
 	print (a)
+	for i in range(n-1):
+		#a,b = b,a+b
+		#vs
+		#a = b
+		#b = a+b
+		a,b = b,a+b
+		print (a)
+
+n = int(input('Fibonacci till :'))
+fibon(n)
