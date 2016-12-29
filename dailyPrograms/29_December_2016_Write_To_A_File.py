eccd70c0a554d0341f53a7b0661bdbf725f6aaf6
Arjun S <arjun.mi1@iiitmk.ac.in>
2016-12-29 06:46:54 +0530

    Done 29_December_2016 Program

--- dailyPrograms/29_December_2016_Write_To_A_File.py
+++ dailyPrograms/29_December_2016_Write_To_A_File.py
@@ -3,9 +3,13 @@ from bs4 import BeautifulSoup
 
 url = "http://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture"
 r = requests.get(url)
-
+open_file = open('file.txt','w')
 soup = BeautifulSoup(r.text,"html5lib")
 for content in soup.find_all(class_="content-section"):
-        for para in content.find_all('p'):
-                for child in para.descendants:
-                        print(child)
+	for para in content.find_all('p'):
+		for child in para.descendants:
+			print(child)
+			open_file.write(str(child))
+
+
+open_file.close()
