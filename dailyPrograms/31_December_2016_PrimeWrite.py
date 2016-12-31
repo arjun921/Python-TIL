b3ada53f0dc8e7bc01d3ce703dbeb81260223293
Arjun S <arjun.mi1@iiitmk.ac.in>
2016-12-31 16:56:31 +0530

    Added Prme writer

---
+++ dailyPrograms/31_December_2016_PrimeWrite.py
@@ -0,0 +1,15 @@
+file_open_prime = open('prime.txt','w')
+
+def storePrime():
+	primenums = int(input("Enter number for prime"))
+	for num in range(1,primenums):
+		prime = True
+		for i in range (2,num):
+			if num%i==0:
+				prime = False
+		if prime:
+			#print(num)
+			file_open_prime.write(str(num))
+	return
+
+storePrime()
