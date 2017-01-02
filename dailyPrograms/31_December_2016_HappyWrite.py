2070954ac3ccc981aed0ad54c08b7c562d0efe4f
Arjun S <arjun.mi1@iiitmk.ac.in>
2017-01-02 13:21:38 +0530

    Still working on 31 December Program

--- dailyPrograms/31_December_2016_HappyWrite.py
+++ dailyPrograms/31_December_2016_HappyWrite.py
@@ -1,16 +1,16 @@
-def happy(n):
-	past = set()
-	while n!=1:
-		n = sum(int(i)**2 for i in str(n))
-		if n in past:
-			return False
-		past.add(n)
-	return True
-
-a = []
-for x in range(1,100):
-	if happy(x)[:8]:
-		a.append(x)
-
-#a = [x for x in range(500) if happy(x)[:8]]
-print (a)
+def is_happy_number():
+	seen = set()
+	a = []
+	for n in range(1,100):
+		status = True
+		while status:
+			digits = [int(c) for c in str(n)]
+			n = sum(digit**2 for digit in digits)
+			if n == 1:
+				a.append(n)
+				status = True
+			elif n in seen:
+				status = False
+			seen.add(n)
+	return a
+print(is_happy_number())
