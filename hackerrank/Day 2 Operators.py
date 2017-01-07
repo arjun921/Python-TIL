fdde72da211c88f2478889a84b986bd21f2929eb
Arjun S <arjun.mi1@iiitmk.ac.in>
2017-01-07 10:26:52 +0530

    Done day 2 Code

---
+++ hackerrank/Day 2 Operators.py
@@ -0,0 +1,6 @@
+mealCost = float(input())
+tipPercent = int(input())
+taxPercent = int(input())
+totalCost = mealCost + (mealCost*(tipPercent/100)) + (mealCost*(taxPercent/100))
+totalCost = round(totalCost)
+print("The total meal cost is {} dollars.".format(int(totalCost)))
