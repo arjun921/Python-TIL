5f7c84c82c0fc645484a3783f487f482fd1667e8
Arjun S <arjun.mi1@iiitmk.ac.in>
2017-01-11 14:05:11 +0530

    Done 11 January Program

--- hackerrank/Day 6: Let's Review.py
+++ hackerrank/Day 6: Let's Review.py
@@ -2,19 +2,19 @@ t = int(input())
 a = []
 even=[]
 odd=[]
-allevs=[]
-allodds=[]
 for x in range(t):
 	a.append(input())
-allevs = []
-for x in a:
-	for i in range(len(x)):
+	
+for x in range(0,len(a)):
+	temp = []
+	for i in range(len(a[x])):
 		if i%2==0:
-			even.append(x[i])
-		else:
-			odd.append(x[i])
-	print (even)
-	print (odd)
-	allevs.append(even)
-	allodds.append(odd)
-
+			temp.append((a[x])[i])
+	even.append("".join(temp))
+	temp2 = []
+	for i in range(len(a[x])):
+		if i%2!=0:
+			temp2.append((a[x])[i])
+	odd.append("".join(temp2))
+for s in zip(even,odd):
+	print(s[0]+ " "+ s[1])
