dfcd3409e5452f06429777f39fbd51427d45759d
Arjun S <arjun.mi1@iiitmk.ac.in>
2017-01-30 14:22:37 +0530

    done question 20

--- classPracticals/20Swedish.py
+++ classPracticals/20Swedish.py
@@ -2,3 +2,10 @@
 {"merry":"god", "christmas":"jul", "and":"och", "happy":gott", "new":"nytt", "year":"år"}
 and use it to translate your Christmas cards from English into Swedish. That is, write a function
 translate() that takes a list of English words and returns a list of Swedish words."""
+
+pyDic = {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"år"}
+
+lis = []
+n = int(input('Enter Number of elements in list:'))
+for x in range(n):
+    lis.append(input('Enter Values:'))
