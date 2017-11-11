ea42491200d0d56d469cf530559e97d0a7d5b188
Arjun S <arjun921@gmail.com>
2017-11-11 16:57:44 +0530

    Update

--- dailyPrograms/2017/november/euler/20171111_problem2.py
+++ dailyPrograms/2017/november/euler/20171111_problem2.py
@@ -1,14 +1,16 @@
 # https://projecteuler.net/problem=2
 a = []
-i = 1
-temp = 1
+i=temp=sum = 1
+bp = int(input())
 a.append(i)
-sum = 0
-while i<=4000000:
+while True:
 	fi = i+temp
 	if fi%2==0:
 		sum += fi
+	if fi>bp:
+		break
 	a.append(fi)
 	i = temp
 	temp = fi
+print(a)
 print(sum)
