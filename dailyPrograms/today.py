2558775f973d8d01487e66838b9f834a6a85c585
arjun921 <arjun921@gmail.com>
2019-01-16 09:07:16 +0530

    updated today.py to open hackerrank with unsolved problems

--- dailyPrograms/today.py
+++ dailyPrograms/today.py
@@ -7,7 +7,8 @@ mm = str(dt.now().month)
 dd = str(dt.now().day)
 a = yy+mm+dd
 
-work_env = input('Where are you working?\n\tw: for office(ubuntu)\n\th: for home(mac)\n')
+# work_env = input('Where are you working?\n\tw: for office(ubuntu)\n\th: for home(mac)\n')
+work_env = 'w'
 if work_env == 'w':
     workingDir_root = Path('/home/alien/working_directory/refactored-lamp/algorithms/')
 elif work_env == 'h':
@@ -24,3 +25,5 @@ if not os.path.exists(workingDir_root / yy / mm / dd):
 os.system('cd {} && code {}.py'.format(workingDir_root / yy / mm / dd,a))
 a = str(workingDir_root / yy / mm / dd )
 print(a + "/")
+# Opens unsolved algorithms page from hackerrank
+os.system('google-chrome https://www.hackerrank.com/domains/algorithms?filters%5Bstatus%5D%5B%5D=unsolved&badge_type=problem-solving')
