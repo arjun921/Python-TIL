5e0c5f96b64f1bbfbc6894109a67a82f7b3732ad
arjun921 <arjun921@gmail.com>
2019-03-15 17:48:59 +0530

    updated steps in algorithm

--- algorithms/2019/3/15/2019315.py
+++ algorithms/2019/3/15/2019315.py
@@ -4,6 +4,8 @@
 ALgorithm
 passed list = [0]
 while in steps range
+    if passed == step_ind:
+        break
     if step is end
         exit
     if step is 1 or if step has passed.
@@ -21,43 +23,62 @@ while in steps range
             jump
             pass
     
+
+    
 return jump
 
 in - 0 0 1 0 0 1 0
-ind- 1 2 3 4 5 6 7
+ind- 0 1 2 3 4 5 6
 
-passed - 1 2 
+passed - 0 0 1 0 0 1 0
+passed - 0 1 2 3 4 5 6
 
-iter 1 === 0
+----------------------------
+iter 0 === 0
 skip2 = 1
 skip1 = 0
 
 skip1 jump
 jump = 1
+passed append 1
 ----------------------------
-iter 2 === 0
+iter 1 === 0
 skip2 = 0
 skip1 = 1
 
 skip2 jump
 jump = 2
+passed append 2
+passed append 3
 ----------------------------
-iter 3 === 1
+iter 2 === 1
 step skip
 ----------------------------
-iter 4 === 0
+iter 3 === 0
 skip2 = 1
 skip1 = 0
 
 skip1 jump
 jump = 3
+passed append 4
+----------------------------
+iter 4 === 0
+skip2 = 0
+skip1 = 1
+
+skip2 jump
+jump = 4
+passed append 4+1=5
+passed append 4+2=6
 ----------------------------
 iter 5 === 0
 skip2 = 0
-skip1 = 
+skip1 = 1
 
 skip2 jump
-jump = 2
+jump = 4
+passed append 4+1=5
+passed append 4+2=6
 
 
 """
