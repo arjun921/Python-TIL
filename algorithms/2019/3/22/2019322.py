4c454a0b271caeecf21eb2913931617f9d4f56e5
Arjun S <arjun921@gmail.com>
2019-03-24 18:39:14 +0530

    added 22-3-19

--- algorithms/2019/3/22/2019322.py
+++ algorithms/2019/3/22/2019322.py
@@ -1,5 +1,6 @@
 # https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrayss
 #!/bin/python3
+# discussions
 
 import math
 import os
@@ -8,23 +9,17 @@ import re
 import sys
 
 # Complete the minimumBribes function below.
-def minimumBribes(q):
-    actual = [x+1 for x in range(len(q))]
-    index_diff = []
-    too_chaotic = False
-    for x in actual:
-        actual_index = actual.index(x)
-        q_index = q.index(x)
-        diff = actual_index-q_index
-        if diff>2:
-            too_chaotic = True
-        elif diff>0 and diff <=2:
-            index_diff.append(diff)
-    if too_chaotic:
-        print('Too chaotic')
-    else:
-        print(index_diff)
-        print(sum(index_diff))
+def minimumBribes(Q):
+    moves = 0 
+    Q = [P-1 for P in Q]
+    for i,P in enumerate(Q):
+        if P - i > 2:
+            print("Too chaotic")
+            return
+        for j in range(max(P-1,0),i):
+            if Q[j] > P:
+                moves += 1
+    print(moves)
 
 if __name__ == '__main__':
     # t = 1
