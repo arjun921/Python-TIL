561e1d31cb70869dfb39380aa2d52d6d7b09639e
Arjun S <arjun921@gmail.com>
2019-03-25 21:31:06 +0530

    Updated gitignore and updated door mat problem

--- algorithms/2019/3/25/2019325-3.py
+++ algorithms/2019/3/25/2019325-3.py
@@ -1,18 +1,40 @@
 # https://www.hackerrank.com/challenges/designer-door-mat/problem
 
-# dimensions = list(map(int,input().split()))
-# max_y = dimensions[0]
-# max_x = dimensions[1]
+dimensions = list(map(int,input().split()))
+max_y = dimensions[0]
+max_x = dimensions[1]
 
-max_x = 21
-max_y = 7
-half = int((max_x-1)/2)
-for y in range(int(max_y-1/2)+1):
-    if y%3 == 0 or y == 1:
+"""Size: 7 x 21 
+---------.|.---------
+------.|..|..|.------
+---.|. .|. .|. .|. .|.---
+-------WELCOME-------
+---.|..|..|..|..|.---
+------.|..|..|.------
+---------.|.---------
+"""
+
+# max_x = 33
+# max_y = 11
+
+def pattern_triangle(y,max_x):
         c = '.|.'*y
-        if len(c) > 0:
-            X = 3*y
-            l = ''.join(['-' for x in range(X)])
-            r = ''.join(['-' for x in range(X)])
-            print(l+c+r)
-    # print('-'*max_y)
+        X = max_x - len(c)
+        X = int(X/2)
+        l = ''.join(['-' for x in range(X)])
+        r = ''.join(['-' for x in range(X)])
+        return (l+c+r)
+
+for y in range(int(max_y-1/2)):
+        if y%2 !=0:                
+                line = pattern_triangle(y,max_x)
+                print(line)
+
+mid = (max_x-len('WELCOME'))/2
+mid = int(mid)
+print('-'*mid +'WELCOME'+ '-'*mid )
+
+for y in range(int(max_y-1/2),0,-1):
+        if y%2 !=0:                
+                line = pattern_triangle(y,max_x)
+                print(line)
