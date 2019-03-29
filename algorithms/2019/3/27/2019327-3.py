afa96e6dd2759cfbcbc7070d38acd346508a08ea
Arjun S <arjun921@gmail.com>
2019-03-29 06:39:06 +0530

    Submission for 27-3

--- algorithms/2019/3/27/2019327-3.py
+++ algorithms/2019/3/27/2019327-3.py
@@ -1,66 +1,31 @@
 # https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
 #!/bin/python3
+# failed
 
-import math
-import os
-import random
-import re
-import sys
+q = int(input())
 
-from collections import Counter
-from itertools import combinations
+for _ in range(q):
+    s = input()
+    signatures = {}
 
-# def get_anagrams(s):
-    
+    # iterate over all substrings of s
+    for start in range(len(s)):
+        for finish in range(start, len(s)):
+            # initialize substring signature
+            # 26 for 26 letters
+            signature = [0 for _ in range(26)]
+            # print(s[start:finish+1])
+            for letter in s[start:finish+1]:
+                signature[ord(letter)-ord('a')] += 1
+            # tuples are hashable in contrast to lists
+            signature = tuple(signature)
+            # print(s[start:finish+1],signature)
+            signatures[signature] = signatures.get(signature, 0) + 1
 
-#     return anagrams_list
-
-# Complete the sherlockAndAnagrams function below.
-def sherlockAndAnagrams(s):
-    leng = len(s)-1
-    pieces = set()
-    for i in range(leng):
-        for j in range(leng+1):
-            print(s[i:j])
-            # if len(combination)>=1:
-            #     pieces.add(combination)
-            #     print(combination)
-    # for i in pieces:
-    count = 0
-    """
-    for all pieces whose length is same, check if each is anagram.
-    # """
-
-    # pieces = list(pieces)
-    # leng = len(pieces)-1
-    # for i in range(0,leng,1):
-    #     for j in range(leng,0,-1):
-    #         f_item = pieces[i]
-    #         b_item = pieces[j]
-    #         # if len(f_item)==len(b_item):
-    #         if Counter(f_item)==Counter(b_item):
-
-    #             print(f_item,b_item)
-        
-
-    # for i,item in enumerate():
-    #     if len(item)==
-    #     # if item[1]==1:
-    #     #     count += 1
-    #     #     print(item)
-    # print(count)
-
-
-
-if __name__ == '__main__':
-
-    # q = int(input())
-
-    # for q_itr in range(q):
-    #     s = input()
-
-    result = sherlockAndAnagrams('abba')
-    # result = sherlockAndAnagrams('ifailuhkqq')
-
-    #     print(str(result))
-
+    # print(signatures)
+    res = 0
+    for count in signatures.values():
+        # print(count)
+        res += count*(count-1)/2
+    # print(res)
+    print(int(res))
