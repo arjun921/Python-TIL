61db66a94710de6523e408d1aa070b77f7a96e83
Arjun Sunil <arjun921@gmail.com>
2021-01-04 09:42:30 +0530

    Implemented Binary search

---
+++ algorithms/2021/1/4/202114.py
@@ -0,0 +1,15 @@
+# https://www.algoexpert.io/questions/Binary%20Search
+
+def binarySearch(array, target):
+	start = 0
+	end = len(array)-1
+	while start<=end:
+		middle = (start+end) // 2
+		potential_match = array[middle]
+		if target==potential_match:
+			return middle
+		elif target<potential_match:
+			end = middle -1
+		else:
+			start = middle + 1
+	return -1
