8a9886d7d605c22b8e73d6d8d5d79d5ee24785b9
Arjun <arjun921@gmail.com>
2026-02-06 09:57:15 +0530

    Add optimal solution for valid palindrome check

--- educative/2026/2/6/202626.py
+++ educative/2026/2/6/202626.py
@@ -1,6 +1,7 @@
 # https://www.educative.io/interview-prep/coding/valid-palindrome-ii
 
 
+# First solution
 def palindrome_check(substring):
     l, r = 0, len(substring) - 1
     while l < r:
@@ -22,3 +23,29 @@ def is_palindrome(string):
             return True
         index += 1
     return False
+
+
+# https://leetcode.com/problems/valid-palindrome-ii/
+# Optimal solution
+
+
+class Solution:
+    def palindrome_check(self, s, left, right):
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+    def validPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return self.palindrome_check(
+                    s, left + 1, right
+                ) or self.palindrome_check(s, left, right - 1)
+
+            left += 1
+            right -= 1
+        return True
