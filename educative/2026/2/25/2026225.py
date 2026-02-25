14876fd43ab2cac5cd91ec0a909b8929af0e02e7
Arjun <arjun921@gmail.com>
2026-02-25 07:25:21 +0530

    Implement function to remove duplicates from sorted array

--- educative/2026/2/25/2026225.py
+++ educative/2026/2/25/2026225.py
@@ -24,3 +24,41 @@ def remove_nth_last_node(head, n):
 
     left.next = left.next.next
     return dummy.next
+
+
+""" Problem 2 """
+
+# https://www.educative.io/interview-prep/coding/solution-remove-duplicates-from-sorted-array
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/1930229177/
+
+
+def removeDuplicates(nums):
+    l = 0
+
+    for r in range(1, len(nums)):
+        if nums[r] != nums[l]:
+            l += 1
+            nums[l] = nums[r]
+
+    # Replace this placeholder return statement with your code
+    return l + 1
+
+
+def main():
+    test_cases = [
+        [1, 1, 2, 2, 3],
+        [-1, -1, 0, 0, 1, 1, 2],
+        [5, 5, 5, 5],
+        [1, 2, 3, 4],
+        [0],
+    ]
+
+    for idx, nums in enumerate(test_cases):
+        print(idx + 1, ".\tnums:", nums, sep="")
+        arr = nums.copy()  # because function modifies in-place
+
+        k = removeDuplicates(arr)
+
+        print("\n\tUnique Count (k):", k)
+        print("\tArray After Removing Duplicates:", arr[:k])
+        print("-" * 100)
